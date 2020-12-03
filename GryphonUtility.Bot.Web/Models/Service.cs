4c3c8dc08baceade6d58bd9be50b244f8086a91d
doroshenkonazar <doroshenkonazar9@gmail.com>
2020-12-04 00:40:03 +0300

    Excess updates types specification removed

--- GryphonUtility.Bot.Web/Models/Service.cs
+++ GryphonUtility.Bot.Web/Models/Service.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
-using Telegram.Bot.Types.Enums;
 
 namespace GryphonUtility.Bot.Web.Models
 {
@@ -12,12 +10,7 @@ namespace GryphonUtility.Bot.Web.Models
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            IEnumerable<UpdateType> allowedUpdates = new List<UpdateType>
-            {
-                UpdateType.Message
-            };
-            return _bot.Client.SetWebhookAsync(_bot.Config.Url, allowedUpdates: allowedUpdates,
-                cancellationToken: cancellationToken);
+            return _bot.Client.SetWebhookAsync(_bot.Config.Url, cancellationToken: cancellationToken);
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
