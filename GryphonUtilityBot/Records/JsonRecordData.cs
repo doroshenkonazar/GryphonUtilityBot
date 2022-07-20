01a22ca6874054efab8f62ba608b9f183362c523
doroshenkonazar <doroshenkonazar9@gmail.com>
2022-07-20 21:38:10 +0400

    Excess interface implementation removed

--- GryphonUtilityBot/Records/JsonRecordData.cs
+++ GryphonUtilityBot/Records/JsonRecordData.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using GoogleSheetsManager;
 using GryphonUtilities;
 using Newtonsoft.Json;
 
 namespace GryphonUtilityBot.Records;
 
-internal sealed class JsonRecordData : IConvertibleTo<RecordData>
+internal sealed class JsonRecordData
 {
     [JsonProperty]
     public int? MessageId { get; set; }
@@ -21,7 +20,11 @@ internal sealed class JsonRecordData : IConvertibleTo<RecordData>
     [JsonProperty]
     public HashSet<string?>? Tags { get; set; }
 
-    public RecordData? Convert()
+    public static List<RecordData>? Convert(List<JsonRecordData?>? data)
+    {
+        return data?.Select(r => r?.Convert()).RemoveNulls().ToList();
+    }
+    private RecordData? Convert()
     {
         if (MessageId is null)
         {
@@ -40,10 +43,5 @@ internal sealed class JsonRecordData : IConvertibleTo<RecordData>
 
         HashSet<string>? tags = Tags is null ? null : new HashSet<string>(Tags.RemoveNulls());
         return new RecordData(MessageId.Value, ChatId.Value, DateTime.Value, tags);
-    }
-
-    public static List<RecordData>? Convert(List<JsonRecordData?>? data)
-    {
-        return data?.Select(r => r?.Convert()).RemoveNulls().ToList();
     }
 }
