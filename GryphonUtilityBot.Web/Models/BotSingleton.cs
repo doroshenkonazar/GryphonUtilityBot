e10938d866c653fc9a76fa824170d779e1b76848
doroshenkonazar <doroshenkonazar9@gmail.com>
2022-11-05 17:26:55 +0200

    Urls list from config to /urls

--- GryphonUtilityBot.Web/Models/BotSingleton.cs
+++ GryphonUtilityBot.Web/Models/BotSingleton.cs
@@ -1,8 +1,39 @@
-﻿namespace GryphonUtilityBot.Web.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GryphonUtilities;
+using Newtonsoft.Json;
+
+namespace GryphonUtilityBot.Web.Models;
 
 public sealed class BotSingleton
 {
     internal readonly Bot Bot;
 
-    public BotSingleton(Config config) => Bot = new Bot(config);
+    public BotSingleton(Config config)
+    {
+        Bot = new Bot(config);
+        PingUrls = GetPingUrls(config).ToList();
+    }
+
+    internal readonly List<Uri> PingUrls;
+
+    private static IEnumerable<Uri> GetPingUrls(Config config)
+    {
+        if (config.PingUrls is not null && (config.PingUrls.Count != 0))
+        {
+            return config.PingUrls.RemoveNulls();
+        }
+
+        if (config.PingUrlsJson is not null)
+        {
+            List<Uri>? deserialized = JsonConvert.DeserializeObject<List<Uri>>(config.PingUrlsJson);
+            if (deserialized is not null)
+            {
+                return deserialized;
+            }
+        }
+
+        return Array.Empty<Uri>();
+    }
 }
--- GryphonUtilityBot.Web/Models/Config.cs
+++ GryphonUtilityBot.Web/Models/Config.cs
@@ -1,5 +1,7 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using JetBrains.Annotations;
 
 // ReSharper disable NullableWarningSuppressionIsUsed
 
@@ -11,4 +13,7 @@ public sealed class Config : GryphonUtilityBot.Config
     [Required]
     [MinLength(1)]
     public string CultureInfoName { get; init; } = null!;
+
+    public List<Uri>? PingUrls { get; init; }
+    public string? PingUrlsJson { get; init; }
 }
