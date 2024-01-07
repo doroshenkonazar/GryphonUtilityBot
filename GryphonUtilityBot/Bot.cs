775cd7debea27614f5ae54b044f7361ad3ee118c
doroshenkonazar <doroshenkonazar9@gmail.com>
2024-01-08 00:30:34 +0100

    Currency removed

--- GryphonUtilityBot/Bot.cs
+++ GryphonUtilityBot/Bot.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using AbstractBot.Bots;
 using AbstractBot.Operations;
 using GryphonUtilities;
@@ -18,7 +17,6 @@ public sealed class Bot : BotWithSheets<Config>
         RecordsManager = new Records.Manager(this, saveManager);
 
         Manager articlesManager = new(this, DocumentsManager);
-        _currencyManager = new Currency.Manager(this);
         InsuranceManager = new InsuranceManager(this);
 
         Operations.Add(new ArticleCommand(this, articlesManager));
@@ -26,7 +24,6 @@ public sealed class Bot : BotWithSheets<Config>
         Operations.Add(new ReadCommand(this, articlesManager));
 
         Operations.Add(new ArticleOperation(this, articlesManager, InsuranceManager));
-        Operations.Add(new CurrencyOperation(this, _currencyManager, InsuranceManager));
         Operations.Add(new FindOperation(this, RecordsManager, InsuranceManager));
         Operations.Add(new ForwardOperation(this));
         Operations.Add(new InsuranceOperation(this, InsuranceManager));
@@ -46,20 +43,9 @@ public sealed class Bot : BotWithSheets<Config>
             replyToMessageId: message.MessageId);
     }
 
-    protected override Task UpdateAsync(CallbackQuery callback)
-    {
-        if (callback.Data is null)
-        {
-            throw new NullReferenceException(nameof(callback.Data));
-        }
-        return _currencyManager.ChangeCurrency(callback.Data);
-    }
-
     internal Records.TagQuery? CurrentQuery;
     internal DateTimeFull CurrentQueryTime;
 
     internal readonly Records.Manager RecordsManager;
     internal readonly InsuranceManager InsuranceManager;
-
-    private readonly Currency.Manager _currencyManager;
 }
