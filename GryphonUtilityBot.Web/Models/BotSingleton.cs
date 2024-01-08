ac29f7ff14ee17c393a69275b4d68eb40bc44d39
doroshenkonazar <doroshenkonazar9@gmail.com>
2024-01-08 03:49:49 +0100

    Ping removed

--- GryphonUtilityBot.Web/Models/BotSingleton.cs
+++ GryphonUtilityBot.Web/Models/BotSingleton.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using GryphonUtilities.Extensions;
 
 namespace GryphonUtilityBot.Web.Models;
 
@@ -10,32 +6,7 @@ public sealed class BotSingleton : IDisposable
 {
     internal readonly Bot Bot;
 
-    public BotSingleton(Config config)
-    {
-        Bot = new Bot(config);
-        PingUrls = GetPingUrls(config).ToList();
-    }
+    public BotSingleton(Config config) => Bot = new Bot(config);
 
     public void Dispose() => Bot.Dispose();
-
-    internal readonly List<Uri> PingUrls;
-
-    private static IEnumerable<Uri> GetPingUrls(Config config)
-    {
-        if (config.PingUrls is not null && (config.PingUrls.Count != 0))
-        {
-            return config.PingUrls.RemoveNulls();
-        }
-
-        if (config.PingUrlsJson is not null)
-        {
-            List<Uri>? deserialized = JsonSerializer.Deserialize<List<Uri>>(config.PingUrlsJson);
-            if (deserialized is not null)
-            {
-                return deserialized;
-            }
-        }
-
-        return Array.Empty<Uri>();
-    }
 }
--- GryphonUtilityBot.Web/Models/Config.cs
+++ GryphonUtilityBot.Web/Models/Config.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using JetBrains.Annotations;
 
@@ -13,10 +12,6 @@ public sealed class Config : GryphonUtilityBot.Config
     [Required]
     [MinLength(1)]
     public string CultureInfoName { get; init; } = null!;
-
-    public List<Uri>? PingUrls { get; init; }
-
-    public string? PingUrlsJson { get; init; }
 
     [Required]
     [MinLength(1)]
